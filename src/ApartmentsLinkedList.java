6e9ab0114cbadaaf6182c342d8f2a69e3a160c8d
Jeremy Rogers <152228726+hullucid@users.noreply.github.com>
2023-12-13 11:10:48 -0600

    Add files via upload

--- src/ApartmentsLinkedList.java
+++ src/ApartmentsLinkedList.java
@@ -1,12 +1,14 @@
 import java.util.LinkedList;
 import java.util.Scanner;
-
+//Jeremy Rogers
+//Course: Programming Fundamentals III
+//Professor: Inetha Sheffield
 
 public class ApartmentsLinkedList {
     public static void main(String[] args) {
         LinkedList<Apartment> apartmentsList = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
-
+        //Prompt user for street address, q being used as a break
         while (true) {
             System.out.print("Enter street address (or enter 'q' to quit): ");
             String streetAddress = scanner.nextLine();
@@ -14,23 +16,24 @@ public class ApartmentsLinkedList {
                 break;
             }
 
+            //prompt user for apartment number
             System.out.print("Enter apartment number: ");
             int apartmentNumber = scanner.nextInt();
-
+            //prompt user for monthly rent
             System.out.print("Enter monthly rent: ");
             double monthlyRent = scanner.nextDouble();
-
+            //prompt user for number of bedrooms
             System.out.print("Enter number of bedrooms: ");
             int numberOfBedrooms = scanner.nextInt();
 
             scanner.nextLine();
-
+            //list for users apartments and apartment information
             Apartment apartment = new Apartment(streetAddress, apartmentNumber, monthlyRent, numberOfBedrooms);
             apartmentsList.add(apartment);
         }
-
+        //sort apartment list information
         apartmentsList.sort(null);
-
+        //print all apartments and their info
         System.out.println("\nSorted list of apartments:");
         for (Apartment apartment : apartmentsList) {
             System.out.println(apartment.toString());
